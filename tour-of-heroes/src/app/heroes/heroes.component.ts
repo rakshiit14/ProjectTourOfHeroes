555be501b7e9219f4dc5e160ce2ba23d0167334c
rakshiit14 <rakshitk30@gmail.com>
2023-05-10 16:59:43 +0530

    Added a message component and service
    Added a component to display message about which hero is clicked and a service to show that message.

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,14 +11,16 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent {
   selectedHero?: Hero;
   heroes:Hero[] = [];
-  constructor(private heroService: HeroService){ }
+  constructor(private heroService: HeroService, private messageService: MessageService){ }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+    .subscribe(heroes =>this.heroes = heroes)
   }
   ngOnInit(){
     this.getHeroes();
   }
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
